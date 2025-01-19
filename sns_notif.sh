5ced978324368e493a81279ef9e303929dee2fd1
Favour Onyeneke <120392002+onlyfave@users.noreply.github.com>
2025-01-19 23:19:37 +0100

    Delete sns_notif.sh

--- sns_notif.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws sns create-topic --name mission-control-alerts
-aws sns subscribe \
-    --topic-arn arn:aws:sns:us-east-1:123456789012:mission-control-alerts \
-    --protocol email \
-    --notification-endpoint devcloudninjas@gmail.com
